7e76b4d8744959e592a7d265b4998168ce14c6c1
maryam99-shuaib <137178921+maryam99-shuaib@users.noreply.github.com>
2023-10-26 13:28:11 +0100

    #

--- index.js
+++ index.js
@@ -1,58 +1,146 @@
-var page_content =  document.getElementById("maincontainer")
-page_content.innerHTML = ` <div class="banner-section">
-                                    <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+var home_content = ` <div class="banner-section">
+                        <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+                    </div>
+                    <div class="SectionThree">
+                        <div class="product_box">
+                            <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+                            <div class="item_description">
+                                <div class="productname">
+                                    PRODUCT NAME
                                 </div>
-                                <div class="SectionThree">
-                                    <div class="box one">
+                                <div class="price_and_button">
+                                    <div class="fivenaira">
+                                        &#8358;<span>5</span>
                                     </div>
-                                    <div class="box two">
-                                    </div>
-                                    <div class="box three">
-                                    </div>
+                                    <button type="button">ADD TO CART</button>
                                 </div>
-                                <button type="button">SUBMIT</button>`
+                            </div>
+                        </div>
+                        <div class="product_box">
+                            <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+                            <div class="item_description">
+                                <div class="productname">
+                                    PRODUCT NAME
+                                </div>
+                                <div class="price_and_button">
+                                <div class="fivenaira">
+                                        &#8358;<span>5</span>
+                                    </div>
+                                    <button type="button">ADD TO CART</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product_box">
+                            <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+                            <div class="item_description">
+                                <div class="productname">
+                                    PRODUCT NAME
+                                </div>
+                                <div class="price_and_button">
+                                <div class="fivenaira">
+                                        &#8358;<span>5</span>
+                                    </div>
+                                    <button type="button">ADD TO CART</button>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <button type="button">SUBMIT</button>`
 
-function updateUI(){
-    
-}
-function pageswitcher(page_name){
-  if(page_name=="home"){
-    page_content.innerHTML = ` <div class="banner-section">
-                                    <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+var product_content = ` <div class="searchbar_container">
+                            <input type="text" id="searchBar">
+                            <button type="button" onclick=""><img src="assessts/fontawesome-free-6.4.0-web/svgs/solid/magnifying-glass.svg"></button>
+                        </div>
+                        <div class="box_set_section">
+                        <div class="product_box">
+                        <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+                        <div class="item_description">
+                            <div class="productname">
+                                PRODUCT NAME
+                            </div>
+                            <div class="price_and_button">
+                                <div class="fivenaira">
+                                    &#8358;<span>5</span>
                                 </div>
-                                <div class="SectionThree">
-                                    <div class="box one">
-                                    </div>
-                                    <div class="box two">
-                                    </div>
-                                    <div class="box three">
-                                    </div>
+                                <button type="button">ADD TO CART</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product_box">
+                        <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+                        <div class="item_description">
+                            <div class="productname">
+                                PRODUCT NAME
+                            </div>
+                            <div class="price_and_button">
+                            <div class="fivenaira">
+                                    &#8358;<span>5</span>
                                 </div>
-                                <button type="button">SUBMIT</button>`
-  }
-  else if(page_name=="product"){
-    page_content.innerHTML = `  <div class="searchbar_container">
-                                    <input type="text" id="searchBar">
-                                    <button type="button" onclick=""><img src="assessts/fontawesome-free-6.4.0-web/svgs/solid/magnifying-glass.svg"></button>
+                                <button type="button">ADD TO CART</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product_box">
+                        <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+                        <div class="item_description">
+                            <div class="productname">
+                                PRODUCT NAME
+                            </div>
+                            <div class="price_and_button">
+                            <div class="fivenaira">
+                                    &#8358;<span>5</span>
                                 </div>
+                                <button type="button">ADD TO CART</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product_box">
+                        <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+                        <div class="item_description">
+                            <div class="productname">
+                                PRODUCT NAME
+                            </div>
+                            <div class="price_and_button">
+                            <div class="fivenaira">
+                                    &#8358;<span>5</span>
+                                </div>
+                                <button type="button">ADD TO CART</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product_box">
+                        <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+                        <div class="item_description">
+                            <div class="productname">
+                                PRODUCT NAME
+                            </div>
+                            <div class="price_and_button">
+                            <div class="fivenaira">
+                                    &#8358;<span>5</span>
+                                </div>
+                                <button type="button">ADD TO CART</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product_box">
+                        <img src="./assessts/images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg">
+                        <div class="item_description">
+                            <div class="productname">
+                                PRODUCT NAME
+                            </div>
+                            <div class="price_and_button">
+                            <div class="fivenaira">
+                                    &#8358;<span>5</span>
+                                </div>
+                                <button type="button">ADD TO CART</button>
+                            </div>
+                        </div>
+                    </div>
+                    
+                        </div>`
 
-                                <div class="box_set_section">
-                                    <div class="set one">
-                                    </div>
-                                    <div class="set two">
-                                    </div>
-                                    <div class="set three">
-                                    </div>
-                                    <div class="set four">
-                                    </div>
-                                    <div class="set five">
-                                    </div>
-                                    <div class="set six">
-                                    </div>
-                                </div>`
-  }
-  else{
-    page_content.innerHTML =  ` <form class="input_section">
+var contact_content = ` <form class="input_section">
                                     <div>
                                         <label for="name">Name</label>
                                         <input type="text" class="inputONE" id="name">
@@ -68,5 +156,21 @@ function pageswitcher(page_name){
                                     </div>
                                     <button type="button">SUBMIT</button>
                                 </form>`
+
+var page_content =  document.getElementById("maincontainer")
+page_content.innerHTML = home_content;
+
+function updateUI(){
+    
+}
+function pageswitcher(page_name){
+  if(page_name=="home"){
+    page_content.innerHTML = home_content
+  }
+  else if(page_name=="product"){
+    page_content.innerHTML = product_content
+  }
+  else{
+    page_content.innerHTML = contact_content 
   }
 }
